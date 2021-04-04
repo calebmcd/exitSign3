97d3f6a0da327a6f304fa84df39d5d8edd6bd310
Caleb McDougall <caleb.mcdougall2@gmail.com>
2021-04-04 16:12:57 -0400

    Fixing some commits

--- main/main.c
+++ main/main.c
@@ -10,3 +10,22 @@
 // -----------------------------------------------------------------------------------------------
 
 #include <led_strip.h>
+
+#define LED_STRIP_LENGTH 17U
+#define LED_STRIP_RMT_INTR_NUM 19U
+
+static struct led_color_t led_strip_buf_1[LED_STRIP_LENGTH];
+static struct led_color_t led_strip_buf_2[LED_STRIP_LENGTH];
+
+struct led_strip_t led_strip = {
+    .rgb_led_type = RGB_LED_TYPE_WS2812,
+    .rmt_channel = RMT_CHANNEL_1,
+    .rmt_interrupt_num = LED_STRIP_RMT_INTR_NUM,
+    .gpio = GPIO_NUM_21,
+    .led_strip_buf_1 = led_strip_buf_1,
+    .led_strip_buf_2 = led_strip_buf_2,
+    .led_strip_length = LED_STRIP_LENGTH
+};
+led_strip.access_semaphore = xSemaphoreCreateBinary();
+
+bool led_init_ok = led_strip_init(&led_strip);
