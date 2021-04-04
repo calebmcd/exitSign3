8465fc020eb79b1c713b46c1cfa6af43a27254d0
Caleb McDougall <caleb.mcdougall2@gmail.com>
2021-04-04 15:23:00 -0400

    Created main.c with description

---
+++ main/main.c
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------------------------------
+//	Project: Exit Sign V3
+//	Created by: Caleb McDougall
+//	Start Date: 3/4/2020
+//	Version: 3.0.0
+//
+//	Description:
+//		IOT/Google Assistant controlled LED exit sign. Built and developed using the
+//		Eclipse ESP32 Toolchain.
+// -----------------------------------------------------------------------------------------------
+
+#include <led_strip.h>
